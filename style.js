c2125d9429a7fab5814ea444b7c2534f208d7de3
Morgane261504 <morgane.phil42@gmail.com>
2021-02-16 18:13:43 +0100

    online 17

---
+++ style.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#typed', {
+    strings: ["Hi, I'm Morgane", "I'm a computer science student","I would like to specialize in cybersecurity"],
+    typeSpeed : 100,
+    backSpeed : 50,
+    loop: true,
+    showCursor: true
+  
+})
